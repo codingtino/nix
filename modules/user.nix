32b227eb8411bfd16c089f974f208ea3db8f2b2f
Tino Naumann <172442418+codingtino@users.noreply.github.com>
2026-08-09 21:30:58 +0200

    Fix user.nix: NixOS only, use security.sudo.extraConfig
    - Remove darwin config (only needed for NixOS)
    - Use security.sudo.extraConfig instead of non-existent wheelNoPassword
    - SSH key and passwordless sudo apply only to NixOS system

--- modules/user.nix
+++ modules/user.nix
@@ -1,27 +1,5 @@
 { lib, ... }:
 {
-  dendritic.darwin."MACOS-NIX" =
-    { userName, ... }:
-    {
-      users.users.${userName} = {
-        home = "/Users/${userName}";
-        shell = "/bin/zsh";
-
-        # Add SSH public key for tino
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9NGlk8rEOSLKF7MsqoeRa/9idlKkPnl/gPNeuTnHe9 MBP"
-        ];
-      };
-
-      # Passwordless sudo for tino
-      security.pam.enableSudoLoginToken = true;
-      security.rootShell = "/bin/zsh";
-
-      environment.etc."sudoers.d/99-tino".text = ''
-        ${userName} ALL=(ALL) NOPASSWD: ALL
-      '';
-    };
-
   dendritic.nixos.NIXOS =
     { userName, ... }:
     lib.mkMerge [
@@ -36,7 +14,9 @@
 
       {
         # Passwordless sudo for tino
-        security.sudo.wheelNoPassword = true;
+        security.sudo.extraConfig = ''
+          ${userName} ALL=(ALL) NOPASSWD: ALL
+        '';
       }
     ];
 }
