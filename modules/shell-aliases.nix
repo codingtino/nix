821fd9e39df9fff7b8b0e062aabd90d9d2ada0e2
Tino Naumann <172442418+codingtino@users.noreply.github.com>
2026-08-11 23:37:56 +0200

    Add platform-aware Nix update alias

---
+++ modules/shell-aliases.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  dendritic.home.default =
+    { pkgs, ... }:
+    let
+      wrapperPath = if pkgs.stdenv.hostPlatform.isDarwin then "/etc/nix-darwin" else "/etc/nixos";
+      rebuildCommand = if pkgs.stdenv.hostPlatform.isDarwin then "darwin-rebuild" else "nixos-rebuild";
+    in
+    {
+      home.shellAliases.nu = "sudo nix flake update --flake ${wrapperPath} nix-config && sudo ${rebuildCommand} switch --flake ${wrapperPath}#system";
+    };
+}
